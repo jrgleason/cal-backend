5eab98fc7a6421a2684cb6cdeff2a844e4342ab5
jrgleason <jackiegleason@gmail.com>
2023-04-22 17:32:18 -0400

    The management API is working and I will need that since I can't delegate to social users using Google

--- src/main/scala/com/secondave/controller/HelloController.scala
+++ src/main/scala/com/secondave/controller/HelloController.scala
@@ -1,10 +1,11 @@
 package com.secondave.controller
 
+import com.auth0.client.mgmt.filter.UserFilter
+import com.auth0.json.mgmt.users.User
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport
 import com.google.api.client.json.gson.GsonFactory
 import com.google.auth.oauth2.GoogleCredentials
-import org.springframework.beans.factory.annotation.Value
-import org.springframework.http.HttpStatus
+import org.springframework.beans.factory.annotation.{Autowired, Value}
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.{GetMapping, RequestHeader, ResponseStatus}
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod.GET
 import org.springframework.web.bind.annotation.{RequestMapping, RestController}
 import com.google.api.services.calendar.{Calendar, CalendarScopes}
 import com.google.auth.http.HttpCredentialsAdapter
+import com.secondave.service.ManagementApiService
 
 import java.io.FileInputStream
 import java.time.LocalDateTime
@@ -22,6 +24,9 @@ import java.util.Collections
 class HelloController:
   private val JSON_FACTORY = GsonFactory.getDefaultInstance
 
+  @Autowired
+  val service: ManagementApiService = null
+
   @Value("${application.name}")
   val appName: String = null
 
@@ -29,7 +34,6 @@ class HelloController:
   def root(): String = {
     val credentials: GoogleCredentials = GoogleCredentials
       .fromStream(new FileInputStream("C:\\Users\\jacki\\Downloads\\second-ave-dffbefd5c686.json"))
-      .createDelegated("jackiegleason@gmail.com")
       .createScoped(Collections.singleton(CalendarScopes.CALENDAR_READONLY))
     credentials.refreshIfExpired()
     val HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport
@@ -37,4 +41,10 @@ class HelloController:
       .setApplicationName(appName)
       .build()
     calendar.calendars().get("primary").execute().getSummary
+  }
+
+  @RequestMapping(path = Array("/auth0"), method = Array(GET))
+  def auth0() = {
+    val mgmt = service.getApi
+    mgmt.users.list(new UserFilter()).execute.getBody
   }
